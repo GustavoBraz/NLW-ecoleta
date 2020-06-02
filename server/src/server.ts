d58c3d5455b230a87782dfaec81d1ec7f43df254
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-01 23:11:30 -0300

    Instala typescript, express e @types/express. Cria rota users.

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+// Para o express no TypeScript é necessária instalação
+// de biblioteca de definição de tipos com o comando:
+// npm install @types/express -D (necessário apenas em
+// desenvolvimento).
+// Em produção não é necessário, pois o TypeScript
+// já terá sido convertido em código JavaScript.
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('Listagem de usuários');
+  response.send('Hello World');
+});
+
+app.listen(33333);
