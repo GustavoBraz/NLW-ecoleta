20fae6c678bad7e0bd4248845f12355d137e2121
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-02 23:54:39 -0300

    Adiciona rota para inserir pontos. Cria atalho para executar seed e ignorar pasta node_modules com --ignore-watch

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,43 @@
-import express from 'express';
+import express, { request } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:33333/uploads/${item.image}`
+    };
+  });
+
+  return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body; // recurso de desestruturação do javascript, exemplo: (const name = request.body.name)
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
 });
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // static é usado para servir arquivos statics de uma determinada pasta
+
 app.listen(33333);
