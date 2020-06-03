fdf76a3b6d5e6d8fa751e710d3343cdd7b084d92
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-02 21:27:20 -0300

    Inclui express.json()

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import express, { request } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 // Rota: Endereço completo da requsição
 // Recurso: Qual entidade estamos acessando do sistema
 
@@ -21,11 +23,15 @@ const app = express();
 // GET http://localhost:3333/users = Listar usuários
 // GET http://localhost:3333/users/5 = Buscar dados do usuário com id 5
 
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação, etc
+// Request Body: Parâmetros para criação/atualização de informações
+
 const users = [
-  'Gustavo',
-  'Eduardo',
-  'Wagner',
-  'Renata'
+  'Gustavo', // 0
+  'Eduardo', // 1
+  'Wagner',  // 2
+  'Renata'   // 3
 ];
 
 app.get('/users', (request, response) => {
@@ -34,10 +40,18 @@ app.get('/users', (request, response) => {
 });
 
 app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
 
+  const user = users[id];
+
+  return response.json(user)
 });
 
 app.post('/users', (request, response) => {
+  const data = request.body;
+  console.log(data);
+
+
   const user = {
     name: 'Gustavo',
     email: 'gustavo.braz89@gmail.com'
