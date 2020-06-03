49b16e96475ba3cd741114b9c216e74af81d3f13
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-03 00:48:58 -0300

    Cria controlers e organiza arquivos para promover menos acoplamento.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,43 +1,13 @@
 import express, { request } from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const itemsController = new ItemsController();
+const pointsController = new PointsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-
-  const serializedItems = items.map(item => {
-    return {
-      title: item.title,
-      image_url: `http://localhost:33333/uploads/${item.image}`
-    };
-  });
-
-  return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body; // recurso de desestruturação do javascript, exemplo: (const name = request.body.name)
-
-  await knex('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  });
-});
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
 
 export default routes;
