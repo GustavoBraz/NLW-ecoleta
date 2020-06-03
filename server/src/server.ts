17c0f80c08eefe743af023a3b3a38e8cd440f567
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-02 21:03:46 -0300

    Adiciona novas rotas e comentarios explicativos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 // Para o express no TypeScript é necessária instalação
 // de biblioteca de definição de tipos com o comando:
 // npm install @types/express -D (necessário apenas em
@@ -8,15 +8,41 @@ import express from 'express';
 
 const app = express();
 
+// Rota: Endereço completo da requsição
+// Recurso: Qual entidade estamos acessando do sistema
+
+
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com id 5
+
+const users = [
+  'Gustavo',
+  'Eduardo',
+  'Wagner',
+  'Renata'
+];
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
+  return response.json(users);
+});
 
-  response.json([
-    'Gustavo',
-    'Eduardo',
-    'Wagner',
-    'Renata'
-  ]);
+app.get('/users/:id', (request, response) => {
+
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Gustavo',
+    email: 'gustavo.braz89@gmail.com'
+  }
+  return response.json(user);
 });
 
 app.listen(33333);
