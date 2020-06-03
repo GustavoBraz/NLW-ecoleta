67feb459e62757e676bb749a71645073c175a4f4
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-02 21:50:23 -0300

    Limpa arquivo para dara sequencia sem comentaros e exemplos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,63 +1,11 @@
 import express, { request } from 'express';
-// Para o express no TypeScript é necessária instalação
-// de biblioteca de definição de tipos com o comando:
-// npm install @types/express -D (necessário apenas em
-// desenvolvimento).
-// Em produção não é necessário, pois o TypeScript
-// já terá sido convertido em código JavaScript.
 
 const app = express();
 
 app.use(express.json());
 
-// Rota: Endereço completo da requsição
-// Recurso: Qual entidade estamos acessando do sistema
-
-
-// GET: Buscar uma ou mais informações do back-end
-// POST: Criar uma nova informação no back-end
-// PUT: Atualizar uma informação no back-end
-// DELETE: Remover uma informação do back-end
-
-// POST http://localhost:3333/users = Criar um usuário
-// GET http://localhost:3333/users = Listar usuários
-// GET http://localhost:3333/users/5 = Buscar dados do usuário com id 5
-
-// Request Param: Parâmetros que vem na própria rota que identificam um recurso
-// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação, etc
-// Request Body: Parâmetros para criação/atualização de informações
-
-const users = [
-  'Gustavo', // 0
-  'Eduardo', // 1
-  'Wagner',  // 2
-  'Renata'   // 3
-];
-
-app.get('/users', (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user)
-});
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-  console.log(data);
-  const user = {
-    name: data.name,
-    email: data.email
-  }
-  return response.json(user);
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
 
 app.listen(33333);
