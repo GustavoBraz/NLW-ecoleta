02589b286dc1acf28507dbd23e6e1bfc9ee1440a
Gustavo Braz <gustavo.braz89@gmail.com>
2020-06-03 23:17:25 -0300

    Cria busca de points utilizando filtros. Adiciona biblioteca cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors()); // Para controlar quais rotas pode acessar aplicação
 app.use(express.json());
 app.use(routes);
 
